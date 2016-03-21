60257e33fcb79c029f5ff1430b556d8aa32e30a0
Grzegorz Terlikowski <grzegorz.terlikowski@nokia.com>
2016-03-22 00:06:56 +0100

    travis update

--- tests/integration/testcase.py
+++ tests/integration/testcase.py
@@ -5,7 +5,7 @@ import securionpay as api
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        api.private_key = os.getenv('PRIVATE_KEY', 'pr_test_FjW0q2HMs40Jj3KGua5294sp')
+        api.private_key = os.getenv('PRIVATE_KEY')
         api.url = os.getenv('URL', 'https://api.securionpay.com')
 
     def tearDown(self):
