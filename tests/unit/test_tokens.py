66367f1abe3d81ceeeb13dcecdfe947173443c24
Grzegorz Terlikowski <grzegorz.terlikowski@nokia.com>
2016-04-06 00:40:16 +0200

    UTs for customer records, events, plans, subscriptions and tokens

---
+++ tests/unit/test_tokens.py
@@ -0,0 +1,18 @@
+import unittest
+from mock import patch
+from securionpay import tokens
+
+
+@patch('securionpay.resource.Resource.request')
+class TestTokens(unittest.TestCase):
+    def test_create(self, request):
+        tokens.create({'some_param': 'some_value'})
+        request.assert_called_once_with('POST',
+                                        '/tokens',
+                                        {'some_param': 'some_value'})
+
+    def test_get(self, request):
+        tokens.get("cusId")
+        request.assert_called_once_with('GET',
+                                        '/tokens/cusId',
+                                        None)
