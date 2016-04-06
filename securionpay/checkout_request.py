cac5c41c1f3d6a8fdf7d0d4f0860a557e582974c
Grzegorz Terlikowski <grzegorz.terlikowski@nokia.com>
2016-04-06 02:22:20 +0200

    python 3 compatability

--- securionpay/checkout_request.py
+++ securionpay/checkout_request.py
@@ -6,7 +6,13 @@ import securionpay as api
 
 
 def sign(checkout_request):
+    try:
+        basestring
+    except NameError:
+        basestring = str
+
     if not isinstance(checkout_request, basestring):
         checkout_request = json.dumps(checkout_request, sort_keys=True, separators=(',', ':'))
+
     digest = hmac.new(api.private_key, msg=checkout_request, digestmod=hashlib.sha256).hexdigest()
     return base64.b64encode(digest + '|' + checkout_request)
