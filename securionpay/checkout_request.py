6a77ada85fa546a4b8cae3134cb8a65d4b626826
Grzegorz Terlikowski <grzegorz.terlikowski@nokia.com>
2016-04-06 03:02:28 +0200

    python 3 compatability

--- securionpay/checkout_request.py
+++ securionpay/checkout_request.py
@@ -9,10 +9,10 @@ def sign(checkout_request):
     try:
         basestring
     except NameError:
-        basestring = str
+        basestring=str
 
     if not isinstance(checkout_request, basestring):
         checkout_request = json.dumps(checkout_request, sort_keys=True, separators=(',', ':'))
 
-    digest = hmac.new(api.private_key, msg=checkout_request, digestmod=hashlib.sha256).hexdigest()
-    return base64.b64encode(digest + '|' + checkout_request)
+    digest = hmac.new(api.private_key.encode(), msg=checkout_request.encode(), digestmod=hashlib.sha256).hexdigest()
+    return base64.b64encode((digest + '|' + checkout_request).encode()).decode()
